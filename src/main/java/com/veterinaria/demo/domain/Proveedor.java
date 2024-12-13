20e5ab7aae9eb0023f9ab77aa63951065b43e167
Andrés <111480424+AndrjsM@users.noreply.github.com>
2024-12-13 15:32:04 -0600

    fix atributos coleecion Proveedores

--- src/main/java/com/veterinaria/demo/domain/Proveedor.java
+++ src/main/java/com/veterinaria/demo/domain/Proveedor.java
@@ -3,18 +3,24 @@ package com.veterinaria.demo.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 
 @Data
 @Document(collection = "Proveedores")
 public class Proveedor {
     @Id
     private String id; // _id en MongoDB 675c7c979e787624f5f437bc
+    @Field("proveedor_id")
     private Integer proveedorId; // proveedor_id 1
+    @Field("nombre_comercial")
     private String nombreComercial; // nombre_comercial Albet
+    @Field("ced_juridica")
     private String cedJuridica; // ced_juridica 310111111
     private String direccion; // direccion SJ
     private String telefono; // telefono 22223333
     private String representante; // representante Andy Blanco
+    @Field("correo_rep")
     private String correoRep; // correo_rep ablanco@albetcr.com
     
     // Constructor
