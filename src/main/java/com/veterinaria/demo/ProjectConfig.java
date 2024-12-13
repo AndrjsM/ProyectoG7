30e9ec4a1744393a83449909ed71a002e3732d01
Andrés <111480424+AndrjsM@users.noreply.github.com>
2024-12-13 10:43:34 -0600

    Inicio configuración de paquetes basicos

---
+++ src/main/java/com/veterinaria/demo/ProjectConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+package com.veterinaria.demo;
+
+/**
+ *
+ * @author andrj
+ */
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+
+}
