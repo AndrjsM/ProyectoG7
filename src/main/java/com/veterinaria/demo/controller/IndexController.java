ad11ec850594f6604bae5cb6a2f0155b3e7f7b16
Andrés <111480424+AndrjsM@users.noreply.github.com>
2024-12-17 09:01:57 -0600

    add registro

--- src/main/java/com/veterinaria/demo/controller/IndexController.java
+++ src/main/java/com/veterinaria/demo/controller/IndexController.java
@@ -1,11 +1,8 @@
-
 package com.veterinaria.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
 @Controller
 public class IndexController {
     
